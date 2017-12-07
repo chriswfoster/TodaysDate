27551122472c48c65f759e725ab13eaccd362dbe
Chris Foster <chriswfoster@gmail.com>
2017-12-07 11:48:54 -0600

    todays date

--- dater.js
+++ dater.js
@@ -2,3 +2,5 @@ I also like linux bash scripting :)
 2017-12-07
 2017-12-07-11:35:48
 Thu Dec  7 11:36:51 CST 2017
+Change the file extension from .js to something else
+I opened my console on Thu Dec  7 11:48:54 CST 2017
